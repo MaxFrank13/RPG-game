9be8f3a899dedacf850906d88e3fdbedc2cc132c
Max <maxfrank13@gmail.com>
2022-01-26 00:18:55 -0500

    added maps and animation loop

---
+++ OverworldMap.js
@@ -0,0 +1,62 @@
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
+
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
+
+  drawLowerImage(ctx) {
+    ctx.drawImage(this.lowerImage, 0, 0)
+  }
+  drawUpperImage(ctx) {
+    ctx.drawImage(this.upperImage, 0, 0)
+  }
+
+}
+
+window.OverworldMaps = {
+  DemoRoom: {
+    lowerSrc: "images/maps/DemoLower.png",
+    upperSrc: "images/maps/DemoUpper.png",
+    gameObjects: {
+      hero: new GameObject({
+        x: 3,
+        y: 1
+      }),
+      npc1: new GameObject({
+        x: 9,
+        y: 2,
+        src: "images/characters/people/npc1.png"
+      }),
+      npc2: new GameObject({
+        x: 4,
+        y: 2,
+        src: "images/characters/people/npc2.png"
+      })
+    }
+  },
+  Kitchen: {
+    lowerSrc: "images/maps/KitchenLower.png",
+    upperSrc: "images/maps/KitchenUpper.png",
+    gameObjects: {
+      hero: new GameObject({
+        x: 3,
+        y: 5
+      }),
+      npc2: new GameObject({
+        x: 9,
+        y: 6,
+        src: "images/characters/people/npc2.png"
+      }),
+      npc3: new GameObject({
+        x: 4,
+        y: 9,
+        src: "images/characters/people/npc3.png"
+      })
+    }
+  }
+}
